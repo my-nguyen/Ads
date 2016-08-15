3f987d7b3d72fbd7266fe84d0b7b192c51ee5868
My Nguyen <My@mys-macbook-pro.local>
2016-08-14 21:54:51 -0700

    finished iAd

--- iAd-Debugging/ViewController.swift
+++ iAd-Debugging/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import iAd
 
-class ViewController: UIViewController {
+// this class conforms to ADBannerViewDelegate in order to set bannerView.delegate to the view controller
+class ViewController: UIViewController, ADBannerViewDelegate {
+
+    var bannerView: ADBannerView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        bannerView = ADBannerView(adType: .Banner)
+        // the translatesAutoresizingMaskIntoConstraints property is to facilitate setting Auto Layout constraints
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        // set the view controller as the delegate of the banner view so it will receive
+        // its load and fail messages
+        bannerView.delegate = self
+        bannerView.hidden = true
+        view.addSubview(bannerView)
+
+        let viewsDictionary = ["bannerView": bannerView]
+
+        // set the iAd banner to fill the full width of the screen and align to the bottom
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[bannerView]|", options: [], metrics: nil, views: viewsDictionary))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[bannerView]|", options: [], metrics: nil, views: viewsDictionary))
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // this method is triggered when an ad is received
+    func bannerViewDidLoadAd(banner: ADBannerView!) {
+        bannerView.hidden = false
+    }
 
+    // this method is triggered when something went wrong
+    func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
+        bannerView.hidden = true
+    }
 }
 
